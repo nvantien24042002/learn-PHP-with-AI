6ca1f49e76dfc996ae50d84f4a659fbe23f56663
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-02 21:59:09 +0700

    learn: practice outputting data in PHP

---
+++ lesson-03-php-output/index.php
@@ -0,0 +1,11 @@
+<?php
+// Xuất dữ liệu echo
+$a = 100;
+echo "Học PHP với AI" ."<br>";
+echo " Giá trị của a: {$a}";
+// Xuất dữ liệu print_f()
+$my__array = array("A","B","C");
+echo "<pre>";
+print_r($my__array);
+echo "</pre>";
+?>
