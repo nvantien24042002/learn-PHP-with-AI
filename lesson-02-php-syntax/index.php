68d50c302ce6b7a75bb8f8f915d2042ed836dc0a
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-02 21:44:32 +0700

    learn: practice basic PHP syntax

---
+++ lesson-02-php-syntax/index.php
@@ -0,0 +1,17 @@
+<?php
+// echo "Hello I am Nguyen Van Tien";
+$name = "Nguyễn Văn Tiến";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cú pháp viết PHP</title>
+</head>
+<body>
+    <h1>Xin chào, tôi là <?php echo $name;?></h1>
+</body>
+</html>
