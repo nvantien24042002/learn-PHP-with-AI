73d301ffac86b15d2826d733aab59d76b26893dc
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-03 13:35:20 +0700

    fix: improve part 3 exercise after review

--- lesson-06-exercise/index.php
+++ lesson-06-exercise/index.php
@@ -1,4 +1,7 @@
 <?php
+// Bước 1:  Xây dựng giao diện
+// Bước 2: Chuẩn bị dữ liệu => Tạo biến 
+// Bước 3: Đổ dữ liệu php lên html
 $fullname = "Nguyen Van Tien";
 $username = "tiennguyen";
 $email = "nguyenvantienthuanan12b3@gmail.com";
@@ -11,6 +14,7 @@ $email = "nguyenvantienthuanan12b3@gmail.com";
     <title>Bài tập 6: Hiển thị thông tin cá nhân</title>
 </head>
 <body>
+    <h1>Thông tin cá nhân</h1>
     <p>Họ và tên <strong><?php echo $fullname;?></strong></p>
     <p>Username <strong><?php echo $username;?></strong></p>
     <p>Email <strong><?php echo $email;?></strong></p>
