f74c574830e30499c3ad477a0aa450839fa0e572
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-03 12:18:32 +0700

    exercise: complete part 3 PHP exercise

---
+++ lesson-06-exercise/index.php
@@ -0,0 +1,18 @@
+<?php
+$fullname = "Nguyen Van Tien";
+$username = "tiennguyen";
+$email = "nguyenvantienthuanan12b3@gmail.com";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài tập 6: Hiển thị thông tin cá nhân</title>
+</head>
+<body>
+    <p>Họ và tên <strong><?php echo $fullname;?></strong></p>
+    <p>Username <strong><?php echo $username;?></strong></p>
+    <p>Email <strong><?php echo $email;?></strong></p>
+</body>
+</html>
