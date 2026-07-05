06b0aafac64cdd9c3e7bf042f679bd0491eb843d
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-05 21:24:37 +0700

    learn: practice PHP arrays

---
+++ part-04-php-data/lesson-05-arrays/docs.md
@@ -0,0 +1,6 @@
+# WHAT IS AN ARRAY?
+- A data type that stores multiple values of different data types in a single variable.
+- Arrays can be one-dimensional or multi-dimensional.
+- An array is an important data type for storing, processing, and displaying information in applications.
+
+$list_course = array('Html Css', 'Jquery', 'Php Mysql');
---
+++ part-04-php-data/lesson-05-arrays/index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+KIỂU DỮ LIỆU MẢNG
+ */
+# Mảng 1 chiều 
+// Mảng số chẵn từ 0 đến 10
+$list_even = array(0,2,4,6,8);
+// Mảng danh sách thành viên 
+/*
+ * Id:.... 
+ * Name:....
+ * Age:....
+ */
+$list_user = array(
+    1=>array(
+        'id'=>1,
+        'fullname'=>"Tien Nguyen",
+        'age'=>30,
+    ),
+    1=>array(
+        'id'=>1,
+        'fullname'=>"Tien Nguyen",
+        'age'=>24,
+    ),
+)
+?>
