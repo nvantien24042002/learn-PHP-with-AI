ca74a8192b5eba3df7fb38649457d79c7989778a
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-07 20:07:44 +0700

    learn: delete PHP array elements

---
+++ part-07-advanced-arrays/lesson-07-delete-array-elements/index.php
@@ -0,0 +1,37 @@
+<?php
+$list_users = array(
+    1 => array(
+        'id' => 1,
+        'fullname' => 'Phan Văn Cương',
+        'email' => 'phancuong.qt@gmail.com'
+    ),
+    2 => array(
+        'id' => 2,
+        'fullname' => 'Nguyễn Hoàng Anh',
+        'email' => 'hoanganh@gmail.com'
+    ),
+    3 => array(
+        'id' => 3,
+        'fullname' => 'Nguyễn Văn Tiến',
+        'email' => 'nguyenvantienthuaan@gmail.com'
+    )
+);
+
+
+// unset($list_users[2]['fullname']);
+// echo "<pre>";
+// print_r($list_users);
+// echo "</pre>";
+
+// unset($list_users[1]);
+// echo "<pre>";
+// print_r($list_users);
+// echo "</pre>";
+
+unset($list_users);
+
+// unset($list_users[1]);
+// echo "<pre>";
+// print_r($list_users);
+// echo "</pre>";
+?>
