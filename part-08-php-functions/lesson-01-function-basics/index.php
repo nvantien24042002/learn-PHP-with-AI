f19df07a980ce85e2e2bad2a38ea93253391214c
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-09 05:02:23 +0700

    feat: function calculateRectangleArea()

--- part-08-php-functions/lesson-01-function-basics/index.php
+++ part-08-php-functions/lesson-01-function-basics/index.php
@@ -3,4 +3,10 @@ function sum($a,$b){
     return $a + $b;
 }
 echo sum(1,2);
+
+function calculateRectangleArea($lenght,$width){
+    return ($lenght + $width) * 2;
+}
+echo "<br>";
+echo calculateRectangleArea(2,5);
 ?>
