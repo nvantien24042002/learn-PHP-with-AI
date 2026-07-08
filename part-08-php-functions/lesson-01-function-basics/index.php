abb0bbef02ca581b5d016b8751a7a25aeca3b6bc
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-09 05:09:11 +0700

    feat: checkEvenOdd()

--- part-08-php-functions/lesson-01-function-basics/index.php
+++ part-08-php-functions/lesson-01-function-basics/index.php
@@ -8,5 +8,9 @@ function calculateRectangleArea($length, $width) {
     return $length * $width;
 }
 echo "<br>";
+// Bài tập 2 : Kiểm tra tính chẵn lẽ
+$n = 3;
+echo ($n % 2 == 0) ? "$n là số chẵn" : "$n là số lẻ";
+echo "<br>";
 echo calculateRectangleArea(2,5);
 ?>
