20bfc121543645353fc6309e92e0b68d5839c5e4
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-15 04:33:47 +0700

    learn: GET Method
    Add exercise 1: basic GET search form

--- part-10-form-data-submission/lesson-02-get-method/exercise_1.php
+++ part-10-form-data-submission/lesson-02-get-method/exercise_1.php
@@ -1,23 +1,22 @@
 <?php
-$keyword = $_GET['keyword'] ?? '';
-echo "Your search for: ". $keyword;
-echo "<br>";
-$id = $_GET['id'] ?? '';
-echo $id;
-// GET data 
+
+if(isset($_GET['keyword']) && $_GET['keyword'] !== ''){
+    $keyword = $_GET['keyword'] ?? '';
+    echo "Your search for: ". $keyword;
+    echo "<br>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exercise_1</title>
 </head>
 <body>
     <form method="GET">
         <input type="text" name="keyword">
         <button type="submit">Search</button>
     </form>
-    <a href="?id=5">View Product 5</a>
 </body>
 </html>
