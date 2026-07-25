aa99589513aaf464e90f43b614f6fc44843c650e
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-25 15:30:40 +0700

    learn : part 13: basic-routing
    feat: complete part 13 basic routing

--- part-13-basic-routing/pages/news.php
+++ part-13-basic-routing/pages/news.php
@@ -1,5 +1,9 @@
+<?php require __DIR__ . '/../lib/data.php'; ?>
 <div id="content">
-    <h1>
-        News Page
-    </h1>
-</div> 
+    <h1>News Page</h1>
+
+    <?php foreach ($news_list as $item) { ?>
+        <h3><?php echo $item['title']; ?></h3>
+        <p><?php echo $item['desc']; ?></p>
+    <?php } ?>
+</div>
--- part-13-basic-routing/pages/product.php
+++ part-13-basic-routing/pages/product.php
@@ -1,5 +1,9 @@
+<?php require __DIR__ . '/../lib/data.php'; ?>
 <div id="content">
-    <h1>
-        Product Page
-    </h1>
-</div> 
+    <h1>Product Page</h1>
+
+    <?php foreach ($product_list as $item) { ?>
+        <h3><?php echo $item['name']; ?></h3>
+        <p><?php echo $item['price']; ?></p>
+    <?php } ?>
+</div>
