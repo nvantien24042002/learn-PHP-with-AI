77f5757ec6fac0c53c44ffa8cdb3be2431098bef
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-08-05 16:28:12 +0700

    feat: create home module

---
+++ product-manager/modules/home/main.php
@@ -0,0 +1,9 @@
+<main>
+    <section>
+        <h1>Welcome to Product Manager</h1>
+        <p>Đây là mini project luyện PHP Router.</p>
+        <a href="?mod=product&act=main">
+            View Products
+        </a>
+    </section>
+</main>
