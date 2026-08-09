2a9a1fb9483a907c8cb4453c21bebad0ce82c22d
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-08-09 18:34:53 +0700

    Add a theme to the project.

---
+++ part-18-shopping-cart/system_advanced/inc/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Basic Routing</title>
+</head>
+<body>
+    <div id="wrapper">
+        <header id="header">
+            <nav>
+                <ul id="main-menu">
+                    <li><a href="?">Home</a></li>
+                    <li><a href="?mod=page&act=detail&id=1">About</a></li>
+                    <li><a href="?mod=post&act=main">News</a></li>
+                    <li><a href="?mod=product&act=main&cat_id=1">Smartphone</a></li>
+                    <li><a href="?mod=product&act=main&cat_id=2">Computer</a></li>
+                    <li><a href="?mod=page&act=detail&id=2">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
