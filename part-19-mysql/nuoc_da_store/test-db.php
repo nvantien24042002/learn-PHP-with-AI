ad5ad59c3fd8988f02712e1a80b289bb0bb27889
Tien Nguyen <87626763+nvantien24042002@users.noreply.github.com>
2026-09-09 13:53:36 +0700

    feat pj nuoc_da_store: add PHP MySQL connection and category query

---
+++ part-19-mysql/nuoc_da_store/test-db.php
@@ -0,0 +1,12 @@
+<?php
+require_once "config/database.php";
+echo "<br>";
+$sql = "SELECT * FROM categories";
+// Gửi thực thi truy vấn
+$result = mysqli_query($conn, $sql);
+// mysqli_fetch_assoc : Lấy từng row từ kết quả
+while ($row = mysqli_fetch_assoc($result)) {
+    echo $row["id"] . " - " . $row["cat_title"] . "<br>";
+}
+// $row : một row dữ liệu
+?>
