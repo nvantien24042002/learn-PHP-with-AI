595851f19d9b88287593a35e0ec7662e5cff50eb
Tien Nguyen <87626763+nvantien24042002@users.noreply.github.com>
2026-09-09 14:44:16 +0700

    done

--- part-19-mysql/nuoc_da_store/test-db.php
+++ part-19-mysql/nuoc_da_store/test-db.php
@@ -1,9 +1,13 @@
 <?php
 require_once "config/database.php";
 echo "<br>";
-$sql = "SELECT * FROM categories WHERE id = 2";
+$id = 1;
+$sql = "SELECT * FROM categories WHERE id = ?";
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
 // Gửi thực thi truy vấn
-$result = mysqli_query($conn, $sql);
+$result = mysqli_stmt_get_result($stmt);
 // mysqli_fetch_assoc : Lấy từng row từ kết quả
 while ($row = mysqli_fetch_assoc($result)) {
     echo $row["id"] . " - " . $row["cat_title"] . "<br>";
